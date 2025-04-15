e581e453495f181384c66a8f021ed1a4b9f6c6cd
PauloAdson <paulo.alves200581@gmail.com>
2025-04-15 01:06:18 -0300

    style: removendo comentário desnecessário

--- src/components/Copyright/Copyright.jsx
+++ src/components/Copyright/Copyright.jsx
@@ -6,12 +6,10 @@ import { SectionContainer } from '../SectionContainer'
 export const Copyright = () => {
   return (
     <Styled.Container>
-      {/* <SectionContainer> */}
       <span>
         PagesLink © {new Date().getFullYear()} | Desenvolvido por{' '}
         <a href="/">Paulo Adson</a>
       </span>
-      {/* </SectionContainer> */}
     </Styled.Container>
   )
 }
